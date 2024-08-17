e447e588d3a9b989acbacb26e8a40dfd5bddba47
rajan-17 <kumar1234.rk16@gmail.com>
2024-08-18 01:20:39 +0530

    Update help_controller.js

--- backend/controller/help_controller.js
+++ backend/controller/help_controller.js
@@ -1,12 +1,30 @@
 const HelpModel = require('../models/help_model')
 
 const add = async(req,res)=> {
-    res.send('add')
+    try {
+        const {title,description} = req.body;
+        if (!title || !description){
+            return res.status(400).send({message:"all fields are mandatory"})
+        }
+        const addNew = new HelpModel({title,description});
+        const resp = await addNew.save();
+        res.status(201).send({message:"help data is add ",resp})
+
+    } catch (error) {
+
+        res.status(500).send({ message: "error", error })
+        
+    }
     
 }
 
 const getall = async(req,res)=> {
-    
+    try {
+        const resp = await HelpModel.find()
+        res.status(200).send(resp)
+    } catch (error) {
+        res.status(500).send({ message: "error", error })
+    }
 }
 
 const search = async(req,res)=> {
